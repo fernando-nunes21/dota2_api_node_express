516ec76c29c59086b0d9527c18f9dea05849abf7
fernando-nunes21 <luis@ubots.com.br>
2021-11-15 23:29:41 -0300

    Ajuste no status das rotas

--- api/route/routes.js
+++ api/route/routes.js
@@ -3,27 +3,27 @@ const heroService = require("../service/heroService");
 
 router.get("/", async function (req, res) {
   const response = await heroService.getHeroes(req);
-  res.json(response);
+  res.status(response.status || 200).json(response);
 });
 
 router.get("/:id", async function (req, res) {
   const response = await heroService.getHeroById(req);
-  res.json(response);
+  res.status(response.status || 200).json(response);
 });
 
 router.post("/", async function (req, res) {
   const response = await heroService.createHero(req);
-  res.json(response);
+  res.status(response.status || 200).json(response);
 });
 
 router.put("/:id", async function (req, res) {
   const response = await heroService.editHero(req);
-  res.json(response);
+  res.status(response.status || 200).json(response);
 });
 
 router.delete("/:id", async function (req, res) {
   const response = await heroService.deleteHero(req);
-  res.json(response);
+  res.status(response.status || 200).json(response);
 });
 
 module.exports = router;
